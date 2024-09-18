6f1a3b90712a45e113bf425b07765632a0f844aa
Matheus Afonso Leite da Silva <matheus.a.leite@hotmail.com>
2024-09-18 12:44:03 -0300

    teste

--- src/main/java/com/projeto01/pokemon/PokemonController.java
+++ src/main/java/com/projeto01/pokemon/PokemonController.java
@@ -1,11 +1,13 @@
 package com.projeto01.pokemon;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class PokemonController {
 
